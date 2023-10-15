060d5bcc4ffb18bc59c7df63dc84a0ef59ec8db8
adambuttrick <adambuttrick@gmail.com>
2023-10-15 13:59:00 -0700

    Add mapping file filtering and updating README

--- utilities/sample_works/sample_works.py
+++ utilities/sample_works/sample_works.py
@@ -12,17 +12,35 @@ def parse_arguments():
                         type=int, help='Number of rows to sample from the CSV.')
     parser.add_argument('-o', '--output_file', default='sampled.csv', type=str,
                         help='Path to the output CSV file. If not provided, a default name will be generated.')
+    parser.add_argument('-m', '--mapping_file', default=None,
+                        help='New line delimited text file. Rows of the CSV will be filtered based on this mapping.')
     return parser.parse_args()
 
 
-def sample_csv(input_file, sample_size):
+def read_mapping(mapping_file):
+    with open(mapping_file, 'r') as f:
+        return f.read().splitlines()
+
+
+def filter_rows_by_mapping(rows, mapping):
+    header, data = rows[0], rows[1:]
+    funder_dois_index = header.index("Funder DOIs")
+    filtered_data = [row for row in data if any(
+        map_val in row[funder_dois_index] for map_val in mapping)]
+    return [header] + filtered_data
+
+
+def sample_csv(input_file, sample_size, mapping=None):
     with open(input_file, 'r') as f:
         reader = csv.reader(f)
         rows = list(reader)
+        if mapping:
+            rows = filter_rows_by_mapping(rows, mapping)
         header, data = rows[0], rows[1:]
         if sample_size > len(data):
             raise ValueError(
                 "Sample size is larger than the number of available rows.")
+
         sampled_data = random.sample(data, sample_size)
     return [header] + sampled_data
 
@@ -35,7 +53,10 @@ def write_to_csv(rows, output_file):
 
 def main():
     args = parse_arguments()
-    sampled_rows = sample_csv(args.input_file, args.sample_size)
+    mapping = None
+    if args.mapping_file:
+        mapping = read_mapping(args.mapping_file)
+    sampled_rows = sample_csv(args.input_file, args.sample_size, mapping)
     output_file = args.output_file
     write_to_csv(sampled_rows, output_file)
 
