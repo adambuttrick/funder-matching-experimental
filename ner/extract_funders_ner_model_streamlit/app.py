b822f2323a03d31216b6fa59ac2734cdd559b950
adambuttrick <adambuttrick@gmail.com>
2024-04-09 23:02:41 -0700

    Add country display

--- ner/extract_funders_ner_model_streamlit/app.py
+++ ner/extract_funders_ner_model_streamlit/app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 from extract_funders_flair import extract_funders_and_ror_ids
 
+
 def main():
     st.title("Extract Funders from Funding Statement")
     st.markdown("Enter a funding statement to extract funder names and ROR IDs.")
-
     funding_statement = st.text_area("Funding Statement", height=200)
-
     if st.button("Extract"):
         if funding_statement.strip():
             with st.spinner("Extracting funders and ROR IDs..."):
@@ -15,12 +14,13 @@ def main():
                     results = extract_funders_and_ror_ids(funding_statement)
                     if results:
                         table_data = []
-                        for org_name, ror_id in results:
+                        for org_name, ror_id, country in results:
                             search_link = f'<a href="https://ror.org/search?query={org_name}" target="_blank">Search</a>'
                             if ror_id:
                                 table_data.append({
                                     "Organisation": org_name,
                                     "ROR ID": f'<a href="{ror_id}" target="_blank">{ror_id}</a>',
+                                    "Country": country if country else "",
                                     "Request": "",
                                     "Search": search_link
                                 })
@@ -28,10 +28,10 @@ def main():
                                 table_data.append({
                                     "Organisation": org_name,
                                     "ROR ID": "",
+                                    "Country": "",
                                     "Request": '<a href="https://curation-request.ror.org" target="_blank">Add record</a>',
                                     "Search": search_link
                                 })
-
                         df = pd.DataFrame(table_data)
                         df = df.fillna("")  # Replace NaN values with empty strings
                         markdown_table = df.to_markdown(index=False)
@@ -42,6 +42,7 @@ def main():
                     st.error(f"An error occurred during extraction: {str(e)}")
         else:
             st.warning("Please enter a funding statement.")
+
 
 if __name__ == "__main__":
     main()
--- ner/extract_funders_ner_model_streamlit/extract_funders_flair.py
+++ ner/extract_funders_ner_model_streamlit/extract_funders_flair.py
@@ -19,23 +19,26 @@ def extract_organisation_names(text):
 
 
 def get_ror_id(org_name):
-    if org_name is None or not re.search("[a-zA-Z]", org_name):
-        return None
-    org_name = re.sub('[{."\\\]', "", org_name)
+    if org_name is None or not re.search(r"[a-zA-Z]", org_name):
+        return None, None
+    org_name = re.sub(r'[{."\\]', "", org_name)
     matched = requests.get(ROR_URL, {"affiliation": org_name})
     if matched.status_code != 200:
-        return None
+        return None, None
     matched = matched.json()
     for matched_org in matched["items"]:
         if matched_org["chosen"]:
-            return matched_org["organization"]["id"]
-    return None
+            ror_id = matched_org["organization"]["id"]
+            country = matched_org["organization"].get(
+                "country", {}).get("country_name")
+            return ror_id, country
+    return None, None
 
 
 def extract_funders_and_ror_ids(text):
     org_names = extract_organisation_names(text)
-    ror_ids = [get_ror_id(org_name) for org_name in org_names]
+    ror_ids_countries = [get_ror_id(org_name) for org_name in org_names]
     names_awards_ids = []
-    for org_name, ror_id in zip(org_names, ror_ids):
-        names_awards_ids.append((org_name, ror_id))
+    for org_name, (ror_id, country) in zip(org_names, ror_ids_countries):
+        names_awards_ids.append((org_name, ror_id, country))
     return names_awards_ids
